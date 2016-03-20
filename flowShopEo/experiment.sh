f6b4f304dfd46844778eaef1e4342874969c4f73
Camilo Martinez <marscamand@gmail.com>
2016-03-21 00:06:09 +0100

    Complete experiment script with padding

--- flowShopEo/experiment.sh
+++ flowShopEo/experiment.sh
@@ -3,25 +3,40 @@
 APP=build/application
 RESULTS=log_fitness.txt
 TIME=log_time.txt
+BINARY=FlowShopEO
+PARAM=$BINARY'.param'
 
 #Cleanup
 (cd $APP && \
  rm -rf Res && \
- rm -f log_*)
+ rm -f log_* && \
+ rm -f $PARAM'.original' && \
+ rm -f $PARAM'.edited')
 
-declare -a instances=('001 054');
+cp $APP/$PARAM $APP/$PARAM'.original'
+
+declare -a instances=('1 23 54');
+#declare -a instances=$(seq 1 120);
 
 echo 'Start experiment'
-for instance in "${instances[@]}"; do
+for instance in ${instances[@]}; do
+    # Pad instance number
+    if [ $instance -lt 10 ]; then
+        instance="00"$instance
+    elif [ $instance -lt 100 ]; then
+        instance="0"$instance
+    fi
+
     new='ta'$instance'.txt'
     # Replace with new instance
-    sed -i -r "s/ta([0-9]{3})\.txt/$new/g" $APP'/FlowShopEO.param'
+    cp $APP/$PARAM'.original' $APP/$PARAM'.edited'
+    sed -i -r "s/ta([0-9]{3})\.txt/$new/g" $APP/$PARAM'.edited'
     benchmark='Benchmark '$instance
     echo "$benchmark" | tee -a $APP/$RESULTS $APP/$TIME
     for run in $(seq 1 5); do
         # Execute
         (cd $APP && \
-         { time mpirun -np 5 FlowShopEO @FlowShopEO.param >> $RESULTS ; } 2>> $TIME )
+         { time mpirun -np 5 $BINARY @"$PARAM"'.edited' >> $RESULTS ; } 2>> $TIME )
         echo 'Finished run #'$run
     done
 done
